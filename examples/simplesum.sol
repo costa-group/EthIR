0c57a27d66f3a64810b53dd2bfd8313d1679fdc3
Pablo Gordillo <pabgordi@ucm.es>
2018-03-05 14:41:36 +0100

    new examples

---
+++ examples/simplesum.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.11;
+
+contract MyContract {
+
+  uint i;
+
+  function sum(){
+    i = 10 + 2 * 2;
+  }
+}
---
+++ examples/sum1.sol
@@ -0,0 +1,13 @@
+
+pragma solidity ^0.4.11;
+
+contract Sum {
+  int sol;
+  int sol1;
+  function suma (int a, int b) returns (int solu) {
+   sol = a+b;
+   sol1 = 5;
+   solu = sol;
+  }
+
+}
