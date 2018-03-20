d8a53e132b72592686a521af08336ba63372b471
Pablo Gordillo <tutugordillo@gmail.com>
2018-03-20 13:38:52 +0100

    init main function

--- oyente/rbr.py
+++ oyente/rbr.py
@@ -1,3 +1,23 @@
 import math
 import sys
-from basicblock import BasicBlock
+
+
+def getKey(block):
+    return block.get_start_address()
+
+def compile_block(block):
+    pass
+
+
+'''
+Main function that build the rbr representation from the CFG of a solidity file.
+It receives as input the blocks of the CFG (basicblock.py)
+'''
+def evm2rbr_compiler(blocks_input = None):
+    if blocks_input :
+        blocks = sorted(blocks_input.values(), key = getKey)
+        for block in blocks:
+            compile_block(block)
+    else :
+        print "Error, you have to provide the CFG associated with the solidity file analyzed"
+
