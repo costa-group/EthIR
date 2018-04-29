c51c3ddf2800a8da5100003022421a4a5fc448f6
tutugordillo <tutugordillo@gmail.com>
2018-04-29 21:14:00 +0200

    new functions

--- oyente/rbr2saco.py
+++ oyente/rbr2saco.py
@@ -13,7 +13,73 @@ def rbr2saco(rbr):
 
 def process_rule_saco(rule):
     pass
-    
+
+def get_contract_vars(rule):
+    bc = rule.get_bc()
+    new = map(lambda x: "l("+x+")",bc)
+    return new
+
+def get_field_vars(rule):
+    gv = rule.get_global_arg()
+    new = map(lambda x: "field("+x+")",gv)
+    return new
+
+def process_instructions(rule):
+    cont = rule.get_fresh_index()
+    instructions = rule.get_instructions()
+    new_instructions = []
+    for instr in instructions:
+        if instr.find("and",0)!=-1:
+            pos = instr.find("=")
+            new = instr[:pos+1]+"s("+cont+")"
+            cont+=1
+        elif instr.find("or",0)!=-1:
+            pos = instr.find("=")
+            new = instr[:pos+1]+"s("+cont+")"
+            cont+=1
+        elif instr.find("not",0)!=-1:
+            pos = instr.find("=")
+            new = instr[:pos+1]+"s("+cont+")"
+            cont+=1
+        elif instr.find("xor",0)!=-1:
+            pos = instr.find("=")
+            new = instr[:pos+1]+"s("+cont+")"
+            cont+=1
+        elif instr.find("gs",0)!=-1:
+            pos = instr.find("=")
+            new = "l("+instr[:pos].strip()+")"+instr[pos:]
+        elif instr.find("gl",0)!=-1:
+            pos = instr.find("=")
+            new = "l("+instr[:pos].strip()+")"+instr[pos:]
+        elif instr.find("ls",0)!=-1:
+            pos = instr.find("=")
+            new = "l("+instr[:pos].strip()+")"+instr[pos:]
+        elif instr.find("ll",0)!=-1:
+            pos = instr.find("=")
+            new = "l("+instr[:pos].strip()+")"+instr[pos:]
+        elif instr.find("call",0)!=-1:
+            pass
+        elif instr.find("fresh",0)!=-1:
+            pos = instr.find("=")
+            new = instr[:pos+1]+"s("+cont+")"
+            cont+=1
+        elif instr.find("= eq",0)!=-1:
+            pos = instr.find("=")
+            new = instr[:pos+1]+"s("+cont+")"
+            cont+=1
+        elif instr.find("= lt",0)!=-1:
+            pos = instr.find("=")
+            new = instr[:pos+1]+"s("+cont+")"
+            cont+=1
+        elif instr.find("= gt",0)!=-1:
+            pos = instr.find("=")
+            new = instr[:pos+1]+"s("+cont+")"
+            cont+=1
+        else:
+            new = instr
+        new_instructions.append(new)
+
+
 def write(rules):
     with open("/tmp/costa/rbr.rbr","w") as f:
         for rule in rules:
