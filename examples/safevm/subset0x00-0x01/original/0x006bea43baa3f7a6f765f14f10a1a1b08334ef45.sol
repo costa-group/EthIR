38580b62565601fe4b809b14a4dfcf94a20f968e
jcorreas <jcorreas@ucm.es>
2019-04-30 11:24:13 +0200

    Update 0x006bea43baa3f7a6f765f14f10a1a1b08334ef45.sol

--- examples/safevm/subset0x00-0x01/original/0x006bea43baa3f7a6f765f14f10a1a1b08334ef45.sol
+++ examples/safevm/subset0x00-0x01/original/0x006bea43baa3f7a6f765f14f10a1a1b08334ef45.sol
@@ -524,4 +524,357 @@ contract StoxSmartToken is SmartToken {
 }
 
 
+/// @title Vesting trustee
+contract Trustee is Ownable {
+    using SaferMath for uint256;
 
+    // The address of the STX ERC20 token.
+    StoxSmartToken public stox;
+
+    struct Grant {
+        uint256 value;
+        uint256 start;
+        uint256 cliff;
+        uint256 end;
+        uint256 transferred;
+        bool revokable;
+    }
+
+    // Grants holder.
+    mapping (address => Grant) public grants;
+
+    // Total tokens available for vesting.
+    uint256 public totalVesting;
+
+    event NewGrant(address indexed _from, address indexed _to, uint256 _value);
+    event UnlockGrant(address indexed _holder, uint256 _value);
+    event RevokeGrant(address indexed _holder, uint256 _refund);
+
+    /// @dev Constructor that initializes the address of the StoxSmartToken contract.
+    /// @param _stox StoxSmartToken The address of the previously deployed StoxSmartToken smart contract.
+    function Trustee(StoxSmartToken _stox) {
+        require(_stox != address(0));
+
+        stox = _stox;
+    }
+
+    /// @dev Grant tokens to a specified address.
+    /// @param _to address The address to grant tokens to.
+    /// @param _value uint256 The amount of tokens to be granted.
+    /// @param _start uint256 The beginning of the vesting period.
+    /// @param _cliff uint256 Duration of the cliff period.
+    /// @param _end uint256 The end of the vesting period.
+    /// @param _revokable bool Whether the grant is revokable or not.
+    function grant(address _to, uint256 _value, uint256 _start, uint256 _cliff, uint256 _end, bool _revokable)
+        public onlyOwner {
+        require(_to != address(0));
+        require(_value > 0);
+
+        // Make sure that a single address can be granted tokens only once.
+        require(grants[_to].value == 0);
+
+        // Check for date inconsistencies that may cause unexpected behavior.
+        require(_start <= _cliff && _cliff <= _end);
+
+        // Check that this grant doesn't exceed the total amount of tokens currently available for vesting.
+        require(totalVesting.add(_value) <= stox.balanceOf(address(this)));
+
+        // Assign a new grant.
+        grants[_to] = Grant({
+            value: _value,
+            start: _start,
+            cliff: _cliff,
+            end: _end,
+            transferred: 0,
+            revokable: _revokable
+        });
+
+        // Tokens granted, reduce the total amount available for vesting.
+        totalVesting = totalVesting.add(_value);
+
+        NewGrant(msg.sender, _to, _value);
+    }
+
+    /// @dev Revoke the grant of tokens of a specifed address.
+    /// @param _holder The address which will have its tokens revoked.
+    function revoke(address _holder) public onlyOwner {
+        Grant grant = grants[_holder];
+
+        require(grant.revokable);
+
+        // Send the remaining STX back to the owner.
+        uint256 refund = grant.value.sub(grant.transferred);
+
+        // Remove the grant.
+        delete grants[_holder];
+
+        totalVesting = totalVesting.sub(refund);
+        stox.transfer(msg.sender, refund);
+
+        RevokeGrant(_holder, refund);
+    }
+
+    /// @dev Calculate the total amount of vested tokens of a holder at a given time.
+    /// @param _holder address The address of the holder.
+    /// @param _time uint256 The specific time.
+    /// @return a uint256 representing a holder's total amount of vested tokens.
+    function vestedTokens(address _holder, uint256 _time) public constant returns (uint256) {
+        Grant grant = grants[_holder];
+        if (grant.value == 0) {
+            return 0;
+        }
+
+        return calculateVestedTokens(grant, _time);
+    }
+
+    /// @dev Calculate amount of vested tokens at a specifc time.
+    /// @param _grant Grant The vesting grant.
+    /// @param _time uint256 The time to be checked
+    /// @return An uint256 representing the amount of vested tokens of a specific grant.
+    ///   |                         _/--------   vestedTokens rect
+    ///   |                       _/
+    ///   |                     _/
+    ///   |                   _/
+    ///   |                 _/
+    ///   |                /
+    ///   |              .|
+    ///   |            .  |
+    ///   |          .    |
+    ///   |        .      |
+    ///   |      .        |
+    ///   |    .          |
+    ///   +===+===========+---------+----------> time
+    ///     Start       Cliff      End
+    function calculateVestedTokens(Grant _grant, uint256 _time) private constant returns (uint256) {
+        // If we're before the cliff, then nothing is vested.
+        if (_time < _grant.cliff) {
+            return 0;
+        }
+
+        // If we're after the end of the vesting period - everything is vested;
+        if (_time >= _grant.end) {
+            return _grant.value;
+        }
+
+        // Interpolate all vested tokens: vestedTokens = tokens/// (time - start) / (end - start)
+         return _grant.value.mul(_time.sub(_grant.start)).div(_grant.end.sub(_grant.start));
+    }
+
+    /// @dev Unlock vested tokens and transfer them to their holder.
+    /// @return a uint256 representing the amount of vested tokens transferred to their holder.
+    function unlockVestedTokens() public {
+        Grant grant = grants[msg.sender];
+        require(grant.value != 0);
+
+        // Get the total amount of vested tokens, acccording to grant.
+        uint256 vested = calculateVestedTokens(grant, now);
+        if (vested == 0) {
+            return;
+        }
+
+        // Make sure the holder doesn't transfer more than what he already has.
+        uint256 transferable = vested.sub(grant.transferred);
+        if (transferable == 0) {
+            return;
+        }
+
+        grant.transferred = grant.transferred.add(transferable);
+        totalVesting = totalVesting.sub(transferable);
+        stox.transfer(msg.sender, transferable);
+
+        UnlockGrant(msg.sender, transferable);
+    }
+}
+
+
+/// @title Stox Smart Token sale
+contract StoxSmartTokenSale is Ownable {
+    using SaferMath for uint256;
+
+    uint256 public constant DURATION = 14 days;
+
+    bool public isFinalized = false;
+    bool public isDistributed = false;
+
+    // The address of the STX ERC20 token.
+    StoxSmartToken public stox;
+
+    // The address of the token allocation trustee;
+    Trustee public trustee;
+
+    uint256 public startTime = 0;
+    uint256 public endTime = 0;
+    address public fundingRecipient;
+
+    uint256 public tokensSold = 0;
+
+    // TODO: update to the correct values.
+    uint256 public constant ETH_CAP = 148000;
+    uint256 public constant EXCHANGE_RATE = 200; // 200 STX for ETH
+    uint256 public constant TOKEN_SALE_CAP = ETH_CAP * EXCHANGE_RATE * 10 ** 18;
+
+    event TokensIssued(address indexed _to, uint256 _tokens);
+
+    /// @dev Throws if called when not during sale.
+    modifier onlyDuringSale() {
+        if (tokensSold >= TOKEN_SALE_CAP || now < startTime || now >= endTime) {
+            throw;
+        }
+
+        _;
+    }
+
+    /// @dev Throws if called before sale ends.
+    modifier onlyAfterSale() {
+        if (!(tokensSold >= TOKEN_SALE_CAP || now >= endTime)) {
+            throw;
+        }
+
+        _;
+    }
+
+    /// @dev Constructor that initializes the sale conditions.
+    /// @param _fundingRecipient address The address of the funding recipient.
+    /// @param _startTime uint256 The start time of the token sale.
+    function StoxSmartTokenSale(address _stox, address _fundingRecipient, uint256 _startTime) {
+        require(_stox != address(0));
+        require(_fundingRecipient != address(0));
+        require(_startTime > now);
+
+        stox = StoxSmartToken(_stox);
+
+        fundingRecipient = _fundingRecipient;
+        startTime = _startTime;
+        endTime = startTime + DURATION;
+    }
+
+    /// @dev Distributed tokens to the partners who have participated during the pre-sale.
+    function distributePartnerTokens() external onlyOwner {
+        require(!isDistributed);
+
+        assert(tokensSold == 0);
+        assert(stox.totalSupply() == 0);
+
+        // Distribute strategic tokens to partners. Please note, that this address doesn't represent a single entity or
+        // person and will be only used to distribute tokens to 30~ partners.
+        //
+        // Please expect to see token transfers from this address in the first 24 hours after the token sale ends.
+        issueTokens(0x9065260ef6830f6372F1Bde408DeC57Fe3150530, 14800000 * 10 ** 18);
+
+        isDistributed = true;
+    }
+
+    /// @dev Finalizes the token sale event.
+    function finalize() external onlyAfterSale {
+        if (isFinalized) {
+            throw;
+        }
+
+        // Grant vesting grants.
+        //
+        // TODO: use real addresses.
+        trustee = new Trustee(stox);
+
+        // Since only 50% of the tokens will be sold, we will automatically issue the same amount of sold STX to the
+        // trustee.
+        uint256 unsoldTokens = tokensSold;
+
+        // Issue 55% of the remaining tokens (== 27.5%) go to strategic parternships.
+        uint256 strategicPartnershipTokens = unsoldTokens.mul(55).div(100);
+
+        // Note: we will substract the bonus tokens from this grant, since they were already issued for the pre-sale
+        // strategic partners and should've been taken from this allocation.
+        stox.issue(0xbC14105ccDdeAadB96Ba8dCE18b40C45b6bACf58, strategicPartnershipTokens);
+
+        // Issue the remaining tokens as vesting grants:
+        stox.issue(trustee, unsoldTokens.sub(strategicPartnershipTokens));
+
+        // 25% of the remaining tokens (== 12.5%) go to Invest.com, at uniform 12 months vesting schedule.
+        trustee.grant(0xb54c6a870d4aD65e23d471Fb7941aD271D323f5E, unsoldTokens.mul(25).div(100), now, now,
+            now.add(1 years), true);
+
+        // 20% of the remaining tokens (== 10%) go to Stox team, at uniform 24 months vesting schedule.
+        trustee.grant(0x4eB4Cd1D125d9d281709Ff38d65b99a6927b46c1, unsoldTokens.mul(20).div(100), now, now,
+            now.add(2 years), true);
+
+        // Re-enable transfers after the token sale.
+        stox.disableTransfers(false);
+
+        isFinalized = true;
+    }
+
+    /// @dev Create and sell tokens to the caller.
+    /// @param _recipient address The address of the recipient.
+    function create(address _recipient) public payable onlyDuringSale {
+        require(_recipient != address(0));
+        require(msg.value > 0);
+
+        assert(isDistributed);
+
+        uint256 tokens = SaferMath.min256(msg.value.mul(EXCHANGE_RATE), TOKEN_SALE_CAP.sub(tokensSold));
+        uint256 contribution = tokens.div(EXCHANGE_RATE);
+
+        issueTokens(_recipient, tokens);
+
+        // Transfer the funds to the funding recipient.
+        fundingRecipient.transfer(contribution);
+
+        // Refund the msg.sender, in the case that not all of its ETH was used. This can happen only when selling the
+        // last chunk of STX.
+        uint256 refund = msg.value.sub(contribution);
+        if (refund > 0) {
+            msg.sender.transfer(refund);
+        }
+    }
+
+    /// @dev Issues tokens for the recipient.
+    /// @param _recipient address The address of the recipient.
+    /// @param _tokens uint256 The amount of tokens to issue.
+    function issueTokens(address _recipient, uint256 _tokens) private {
+        // Update total sold tokens.
+        tokensSold = tokensSold.add(_tokens);
+
+        stox.issue(_recipient, _tokens);
+
+        TokensIssued(_recipient, _tokens);
+    }
+
+    /// @dev Fallback function that will delegate the request to create.
+    function () external payable onlyDuringSale {
+        create(msg.sender);
+    }
+
+    /// @dev Proposes to transfer control of the StoxSmartToken contract to a new owner.
+    /// @param _newOwnerCandidate address The address to transfer ownership to.
+    ///
+    /// Note that:
+    ///   1. The new owner will need to call StoxSmartToken's acceptOwnership directly in order to accept the ownership.
+    ///   2. Calling this method during the token sale will prevent the token sale to continue, since only the owner of
+    ///      the StoxSmartToken contract can issue new tokens.
+    function transferSmartTokenOwnership(address _newOwnerCandidate) external onlyOwner {
+        stox.transferOwnership(_newOwnerCandidate);
+    }
+
+    /// @dev Accepts new ownership on behalf of the StoxSmartToken contract. This can be used, by the token sale
+    /// contract itself to claim back ownership of the StoxSmartToken contract.
+    function acceptSmartTokenOwnership() external onlyOwner {
+        stox.acceptOwnership();
+    }
+
+    /// @dev Proposes to transfer control of the Trustee contract to a new owner.
+    /// @param _newOwnerCandidate address The address to transfer ownership to.
+    ///
+    /// Note that:
+    ///   1. The new owner will need to call Trustee's acceptOwnership directly in order to accept the ownership.
+    ///   2. Calling this method during the token sale won't be possible, as the Trustee is only created after its
+    ///      finalization.
+    function transferTrusteeOwnership(address _newOwnerCandidate) external onlyOwner {
+        trustee.transferOwnership(_newOwnerCandidate);
+    }
+
+    /// @dev Accepts new ownership on behalf of the Trustee contract. This can be used, by the token sale
+    /// contract itself to claim back ownership of the Trustee contract.
+    function acceptTrusteeOwnership() external onlyOwner {
+        trustee.acceptOwnership();
+    }
+}
