ae8ef12fa7b0581049bbe1c306fb8852ac185d36
Pablo <tutugordillo@gmail.com>
2019-10-23 00:31:50 +0200

    fixing name field

--- ethir/utils.py
+++ ethir/utils.py
@@ -472,7 +472,7 @@ def update_sstore_map(state_vars,initial_name,compressed_name,isCompresed,positi
             #print compressed_name
             if initial_name != compressed_name:
                 compressed = get_field_from_string(compressed_name,state)
-                r_val = (initial_name,compressed_name)
+                r_val = (initial_name,compressed_name.split()[-1])
  
             state_vars[position] = initial_name
         else:
