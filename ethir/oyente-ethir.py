9d2e10edf745154c376f099418f8855fb0f7be87
tutugordillo <tutugordillo@gmail.com>
2019-10-23 15:47:55 +0200

    deleting .log

--- ethir/oyente-ethir.py
+++ ethir/oyente-ethir.py
@@ -72,7 +72,7 @@ def has_dependencies_installed():
     return True
 
 def clean_dir():
-    ext = ["rbr","cfg","txt","config","dot","csv","c","pl"]
+    ext = ["rbr","cfg","txt","config","dot","csv","c","pl","log"]
     if "costabs" in os.listdir(tmp_path):
         for elem in os.listdir(costabs_path):
             last = elem.split(".")[-1]
