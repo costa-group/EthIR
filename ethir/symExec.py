40c0fb133f5c13722088ff501428c8340ba91a0c
Pablo <tutugordillo@gmail.com>
2022-03-21 13:16:28 +0100

    vertices_memory map

--- ethir/symExec.py
+++ ethir/symExec.py
@@ -3747,6 +3747,11 @@ def run(disasm_file=None, disasm_file_init=None, source_map=None, source_map_ini
         #     get_block_type: conditional, unconditional,falls_to, terminal
         # comes_from en basic_block: identificador de todos los bloques desde los que puedes llegar
 
+        vertices_memory = {}
+        for v in vertices:
+            vertices_memory[str(v)] = vertices[v]
+
+        #use vertices_memory for mem. Keys are strings
         
         rbr_rules = rbr.evm2rbr_compiler(blocks_input = vertices,stack_info = stack_h, block_unbuild = blocks_to_create,saco_rbr = saco,c_rbr = cfile, exe = execution, contract_name = cname, component = component_of_blocks, oyente_time = oyente_t,scc = scc,svc_labels = svc,gotos = go,fbm = f2blocks, source_info = source_info,mem_abs = (mem_abs,storage_arrays,mapping_address_sto,val_mem40),sto = sto)
         
