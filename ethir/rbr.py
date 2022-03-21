ee3e553f63e3a5805b85fd0f64b3b4edccc06069
Pablo <tutugordillo@gmail.com>
2022-03-21 13:39:58 +0100

    prueba

--- ethir/rbr.py
+++ ethir/rbr.py
@@ -1787,6 +1787,8 @@ def evm2rbr_compiler(blocks_input = None, stack_info = None, block_unbuild = Non
                 init_fields_def ={}
 
 
+            print(mem_creation)
+                
             # print "********************************************"
             # print storage_arrays
             if saco_rbr:
