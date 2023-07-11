dae2c6f4340666be3afa7a922102d95842fbfa47
Pablo Gordillo <pabgordi@ucm.es>
2023-07-11 11:57:11 +0200

    global_params -> global_params_ethir

--- ethir/c_utranslation.py
+++ ethir/c_utranslation.py
@@ -2,7 +2,7 @@ from rbr_rule import RBRRule
 import os
 from timeit import default_timer as dtimer
 from utils import delete_dup, is_executed_by
-import global_params
+import global_params_ethir
 import  traceback
 
 '''
@@ -2473,11 +2473,11 @@ def write_init(rules,execution,cname,num_mem_vars):
         s = s+"\n"
         
     if execution == None:
-        name = global_params.costabs_path+"rbr.c"
+        name = global_params_ethir.costabs_path+"rbr.c"
     elif cname == None:
-        name = global_params.costabs_path+"rbr"+str(execution)+".c"
+        name = global_params_ethir.costabs_path+"rbr"+str(execution)+".c"
     else:
-        name = global_params.costabs_path+cname+".c"
+        name = global_params_ethir.costabs_path+cname+".c"
     with open(name,"w") as f:
         if(len(rules)>1):
             r = rules[1][0]
@@ -2820,11 +2820,11 @@ def update_stack_vars_global(vs):
 
 def write_main(execution,cname,init_vars):
     if execution == None:
-        name = global_params.costabs_path+"rbr.c"
+        name = global_params_ethir.costabs_path+"rbr.c"
     elif cname == None:
-        name = global_params.costabs_path+"rbr"+str(execution)+".c"
+        name = global_params_ethir.costabs_path+"rbr"+str(execution)+".c"
     else:
-        name = global_params.costabs_path+cname+".c"
+        name = global_params_ethir.costabs_path+cname+".c"
 
     if not svcomp["exec"]:
         with open(name,"a") as f:
@@ -2852,11 +2852,11 @@ def write(head,rules,execution,cname):
     #     os.mkdir("/tmp/costabs/")
 
     if execution == None:
-        name = global_params.costabs_path+"rbr.c"
+        name = global_params_ethir.costabs_path+"rbr.c"
     elif cname == None:
-        name = global_params.costabs_path+"rbr"+str(execution)+".c"
+        name = global_params_ethir.costabs_path+"rbr"+str(execution)+".c"
     else:
-        name = global_params.costabs_path+cname+".c"
+        name = global_params_ethir.costabs_path+cname+".c"
     with open(name,"a") as f:
         f.write(head+"\n")
         
@@ -3007,9 +3007,9 @@ def vars_in_main(fields,local,blockchain,num):
 def get_invalids_entry_functions(components_of, rbr,cname):
 
     if cname == None:
-        name = global_params.costabs_path+"config_block.config"
+        name = global_params_ethir.costabs_path+"config_block.config"
     else:
-        name = global_params.costabs_path+cname+".config"
+        name = global_params_ethir.costabs_path+cname+".config"
 
 
     f = open(name,"r")
@@ -3178,11 +3178,11 @@ def process_blocks_ethirui_recur(rbr,new_rule):
 
 def write_ethirui_config(cname,execution):
     if execution == None:
-        name = global_params.costabs_path+"rbr.ethirui"
+        name = global_params_ethir.costabs_path+"rbr.ethirui"
     elif cname == None:
-        name = global_params.costabs_path+"rbr"+str(execution)+".ethirui"
+        name = global_params_ethir.costabs_path+"rbr"+str(execution)+".ethirui"
     else:
-        name = global_params.costabs_path+cname+".ethirui"
+        name = global_params_ethir.costabs_path+cname+".ethirui"
     with open(name,"w") as f:
         lines = "\n".join(mapping_ethirui)
         f.write(lines)
