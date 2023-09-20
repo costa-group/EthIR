4430db479aaee79d5d09dd9877848b4d2e922305
Nicolas Alonso <nicolas.alonso.shirra@alumnos.upm.es>
2023-09-20 11:51:00 +0200

    Prueba de funcionamiento de librerias

---
+++ examples/ext_library.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.8.0;
+
+import "./library.sol";
+
+contract Extern{
+  
+  function enter() public {
+    uint x = Math.pow(1, 2);
+  }
+
+}
---
+++ examples/library.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+library Math {
+    function pow(uint a, uint b) public view returns (uint){
+        return a ** b;
+    }
+}
