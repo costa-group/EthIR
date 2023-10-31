c1b21ccdf86cb653215329511db627062cb8aea0
Nicolas Alonso <nicolas.alonso.shirra@alumnos.upm.es>
2023-10-31 09:57:38 +0100

    Reformateo de las clases jump_origin_analysis y sequence

--- ethir/jump_origin_analysis.py
+++ ethir/jump_origin_analysis.py
@@ -6,12 +6,17 @@ global K
 K = 10000
 
 class JumpOriginAbstractState:          
-    stack_pos = None
     ssequence = Sequence(sequence = ("PUSH2","EXP","DUP2","SLOAD","DUP2","PUSH8","MUL","NOT","AND","SWAP1","DUP4","PUSH8","AND","MUL","OR","SWAP1","SSTORE","POP"), stack_position = 4)
     lsequence = Sequence(sequence = ("PUSH2","EXP","SWAP1","DIV","DUP1","ISZERO","PUSH","MUL","OR","PUSH8","AND","PUSH4","AND","JUMP"), stack_position=3)
+
+    stack_next_position: int
+    stack: dict
+    jump_directions: list
+    debug: bool
+    storage: dict
     
-    def __init__(self,stack_pos,stack,storage,debug,jump_directions:list):
-        self.stack_pos = stack_pos
+    def __init__(self,stack_pos: int,stack: dict,storage: dict,debug: bool,jump_directions:list):
+        self.stack_next_position = stack_pos
         self.stack = stack
         self.jump_directions = jump_directions
         self.debug = debug
@@ -19,8 +24,8 @@ class JumpOriginAbstractState:
 
 
 
-    def leq(self, state):
-        if self.stack_pos != state.stack_pos: 
+    def leq(self, state: 'JumpOriginAbstractState') -> bool:
+        if self.stack_next_position != state.stack_next_position: 
             print("CONSTANT ANALYSIS WARNING: Different stacks in leq !!! ")
             print("CONSTANT ANALYSIS WARNING: " + str(self))
             print("CONSTANT ANALYSIS WARNING: " + str(state))
@@ -40,15 +45,15 @@ class JumpOriginAbstractState:
 
         return True
 
-    def lub(self, state):
+    def lub(self, state: 'JumpOriginAbstractState')-> 'JumpOriginAbstractState':
         if self.debug:
             print ("DOING LUB: " + str(self.stack) + " " + str(state.stack))
-        if self.stack_pos != state.stack_pos: 
+        if self.stack_next_position != state.stack_next_position: 
             print("CONSTANT ANALYSIS WARNING: Different stacks in lub !!! ")
             print("CONSTANT ANALYSIS WARNING: " + str(self))
             print("CONSTANT ANALYSIS WARNING: " + str(state))
 
-        res_stack = self.stack.copy(); 
+        res_stack = self.stack.copy()
 
         for skey in state.stack: 
             if skey in res_stack: 
@@ -56,9 +61,9 @@ class JumpOriginAbstractState:
             else:
                 res_stack[skey] = state.stack[skey]
 
-        return JumpOriginAbstractState(self.stack_pos, res_stack, self.storage, self.debug, self.jump_directions)
+        return JumpOriginAbstractState(self.stack_next_position, res_stack, self.storage, self.debug, self.jump_directions)
 
-    def process_instruction(self, instr: str, pc):
+    def process_instruction(self, instr: str, pc) -> 'JumpOriginAbstractState':
         
         op_code = instr.split()[0]
 
@@ -67,40 +72,38 @@ class JumpOriginAbstractState:
         opinfo = get_opcode(op_code)
         stack_in = opinfo[1]
         stack_out = opinfo[2]
-        stack_res = self.stack_pos - stack_in + stack_out
-        top = self.stack_pos-1
+        stack_result_length = self.stack_next_position - stack_in + stack_out
+        top = self.stack_next_position-1
 
-        self.ssequence.is_in_sequence(op_code, stack)
-        self.lsequence.is_in_sequence(op_code, stack)
+        self.ssequence.register_instruction(op_code, stack)
+        self.lsequence.register_instruction(op_code, stack)
 
         treated = False
 
         if op_code.startswith("PUSH"):
             strvalue = instr.split()[1]
             value = int(strvalue, 16)
+
             if 0 <= value < K:
-                stack[self.stack_pos] = set({value})
+                stack[self.stack_next_position] = set({value})
             else:
-                stack[self.stack_pos] = set({'*'})
-            treated = True
-        
-        
-        elif op_code == "POP":
-            stack.pop(top,None)
+                stack[self.stack_next_position] = set({'*'})
             treated = True
 
         elif op_code.startswith("DUP"):
             position = top - int(op_code[3:], 10)+1
+
             if position in stack:
-                stack[self.stack_pos] = stack[position]
+                stack[self.stack_next_position] = stack[position]
             treated = True
         
         elif op_code.startswith("SWAP",0):
             position = top-int(op_code[4:], 10)
-            if position in stack and not(top in stack):
+
+            if position in stack and top not in stack:
                 stack[top] = stack[position] 
                 stack.pop(position,None)
-            elif top in stack and not(position in stack):
+            elif top in stack and position not in stack:
                 stack[position] = stack[top]
                 stack.pop(top,None) 
             elif top in stack and position in stack:
@@ -110,54 +113,67 @@ class JumpOriginAbstractState:
                 treated = True
 
         elif op_code == "JUMP":
-            if self.lsequence.get_storage_value() is None:
+            sloaded_values = self.lsequence.get_storage_value()
+
+            if sloaded_values is None:
                 direction = stack.pop(top)
             else:
-                direction = set(self.lsequence.get_storage_value())
+                direction = sloaded_values
                 stack.pop(top)
+
             print(f"Jump direction is {direction}")
             self.jump_directions.append(direction)
             treated = True
 
         elif op_code == "JUMPI":
-            direction = stack.pop(top)
+            sloaded_values = self.lsequence.get_storage_value()
+
+            if sloaded_values is None:
+                direction = stack.pop(top)
+            else:
+                direction = sloaded_values
+                stack.pop(top)
             top -= 1
             stack.pop(top)
+
             print(f"Jumpi direction is {direction}")
             self.jump_directions.append(direction)
             treated = True
 
         elif op_code.startswith("SSTORE") and not instr.endswith('?'):
-            if self.ssequence.get_storage_value() is None:
-                direction = stack.pop(top)
+            sstore_values = self.ssequence.get_storage_value()
+            if sstore_values is None:
+                stack.pop(top) #direction
                 top -= 1
-                values = stack.pop(top)
-            else:
-                values = self.ssequence.get_storage_value()
-                stack.pop(top)
+                values = stack.pop(top)#value
                 top -= 1
-                stack.pop(top)
-            top -= 1
-            direction = instr.split()[1].split('_')
+            else:
+                values = sstore_values
+                stack.pop(top)#direction
+                top -= 1
+                stack.pop(top)#value
+                top -= 1
+            
+            direction = instr.split()[1].split('_') # instruction can be in the form of either SSTORE 0 or SSTORE 0_0
 
-            if self.storage.get(direction[0]) is not None:
+            if self.storage.get(direction[0]) is None:
                 if len(direction) == 1:
-                    self.storage[direction[0]].union(values)
+                    self.storage[direction[0]] = values
                 else:
-                    self.storage[direction[0]][direction[1]] = set(values)
+                    self.storage[direction[0]] = {direction[1]: values}
 
             else:
                 if len(direction) == 1:
-                    self.storage[direction[0]] = set(values)
+                    self.storage[direction[0]] = self.storage[direction[0]].union(values)
                 else:
-                    self.storage[direction[0]] = {direction[1]: set(values)}
+                    self.storage[direction[0]][direction[1]] = values
             treated = True
 
         elif op_code.startswith("SLOAD") and not instr.endswith('?'):
             direction = instr.split()[1]
             value = self.storage.get(direction)
             if value is None:
-                stack[top] = {value}
+                stack[top] = {None}
             else:
                 stack[top] = value
             top += 1
@@ -166,16 +182,16 @@ class JumpOriginAbstractState:
 
 
         if not treated:
+            # eliminates the positions used by the instruction if stack_in > stack_out
             for i in range(stack_in):
-                stack.pop(self.stack_pos-1-i,None)
-            self.stack_pos -=stack_in
+                stack.pop(self.stack_next_position-1-i,None)
+            self.stack_next_position -=stack_in
+            # fills the new positions with unknown if stack_out > stack_in
             for i in range(stack_out):
-                stack[self.stack_pos] = set({"*"})
-                self.stack_pos += 1
-        else:
-            self.stack_pos = min(stack_res, self.stack_pos)
+                stack[self.stack_next_position] = set({"*"})
+                self.stack_next_position += 1
         
-        return JumpOriginAbstractState(stack_res, stack, self.storage, self.debug, self.jump_directions)
+        return JumpOriginAbstractState(stack_result_length, stack, self.storage, self.debug, self.jump_directions)
     
     def __repr__(self):
-        return (" stack^" + str(self.stack_pos) + " = " + str(self.stack))
+        return (" stack^" + str(self.stack_next_position) + " = " + str(self.stack))
--- ethir/sequence.py
+++ ethir/sequence.py
@@ -2,40 +2,45 @@ class Sequence:
 
     sequence: tuple
     instruction_index: int
-    storage_value: int
+    storage_value: set
     stack_position: int
-    displacement: int
+    displacement: set
 
-    def __init__(self, sequence, stack_position) -> None:
+    def __init__(self, sequence: tuple, stack_position: int) -> None:
         self.instruction_index = 0
         self.storage_value = None
         self.sequence = sequence
         self.stack_position = stack_position
         self.displacement = 0
     
-    def is_in_sequence(self, instruction: str, stack):
+    def register_instruction(self, instruction: str, stack: dict):
         if instruction.startswith(self.sequence[self.instruction_index]):
+
+            # displacement in case of sload
             if self.instruction_index == 0:
                 self.displacement = stack[len(stack) -1]
+            
+            # stack direction for sstore and sload
             if self.storage_value is None and self.instruction_index == 1:
                 stack_value = stack[len(stack) - self.stack_position]
+
+                # load the correct values from storage
                 if isinstance(stack_value, dict):
-                    value_list = []
+                    values = set()
                     for displacement in self.displacement:
-                        value_list += list(stack_value[str(displacement)])
+                        values = values.union(stack_value[str(displacement)])
                     
-                    self.storage_value = value_list
+                    self.storage_value = values
                 else:
-                    self.storage_value = list(stack_value)
+                    self.storage_value = stack_value
+
             self.instruction_index = (self.instruction_index + 1)%len(self.sequence)
-            return True
 
         
         self.instruction_index = 0
         self.storage_value = None
-        return False
 
-    def get_storage_value(self):
+    def get_storage_value(self) -> set:
         return self.storage_value
 
     def __repr__(self) -> str:
