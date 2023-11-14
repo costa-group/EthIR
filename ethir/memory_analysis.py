4079f83f000ee7b2f5b2256b5a8c32f107b4785c
epichalcon <nicolashirra115@gmail.com>
2023-11-14 11:17:18 +0100

    correcciones en leq y lub

--- ethir/memory_analysis.py
+++ ethir/memory_analysis.py
@@ -15,75 +15,88 @@ import os
 
 global debug_info
 
-class BlockAnalysisInfo: 
 
+class BlockAnalysisInfo:
     ## Creates an initial abstract state with the received information
-    def __init__ (self, block_info, input_state): 
+    def __init__(self, block_info, input_state):
         self.block_info = block_info
         self.input_state = input_state
         self.output_state = None
         self.state_per_instr = []
 
-    def get_input_state (self): 
+    def get_input_state(self):
         return self.input_state
 
-    def get_output_state (self): 
-        return self.output_state  
+    def get_output_state(self):
+        return self.output_state
 
-    def get_state_at_instr (self,pos): 
+    def get_state_at_instr(self, pos):
         return self.state_per_instr[pos]
 
     ## Evaluates if a block need to be revisited or not
-    def revisit_block (self,input_state, jump_target): 
+    def revisit_block(self, input_state, jump_target):
         leq = input_state.leq(self.input_state)
 
-        if leq: 
+        if leq:
             return False
         self.input_state = self.input_state.lub(input_state)
         del self.state_per_instr[:]
         return True
-        
-    def process_block (self):
 
+    def process_block(self):
         # We start with the initial state of the block
         current_state = self.input_state
         idblock = self.block_info.get_start_address()
 
         if debug_info:
-            print("\n\nProcessing " + str(idblock) + 
-            " :: " + str(current_state) + 
-            " -- " + str(self.block_info.get_stack_info()))
-        
+            print(
+                "\n\nProcessing "
+                + str(idblock)
+                + " :: "
+                + str(current_state)
+                + " -- "
+                + str(self.block_info.get_stack_info())
+            )
+
         i = 0
         instructions = self.block_info.get_instructions()
 
-        for i, instr in enumerate(instructions): 
+        for i, instr in enumerate(instructions):
             # From the current state we generate a new state by processing the instruction
-            current_state = current_state.process_instruction(instr, str(idblock) + ":" + str(i))
+            current_state = current_state.process_instruction(
+                instr, str(idblock) + ":" + str(i)
+            )
             if debug_info:
-                print(f"      -- {self.block_info.get_start_address()}[{i}] {instr} -- {current_state}")
+                print(
+                    f"      -- {self.block_info.get_start_address()}[{i}] {instr} -- {current_state}"
+                )
             self.state_per_instr.append(current_state)
 
         self.output_state = current_state
-    
+
     def __repr__(self):
-
         i = 0
-        for state in self.state_per_instr: 
-            print (str(self.block_info.get_start_address()) + "." + str(i) + ": " + str(self.state_per_instr[i]))
+        for state in self.state_per_instr:
+            print(
+                str(self.block_info.get_start_address())
+                + "."
+                + str(i)
+                + ": "
+                + str(self.state_per_instr[i])
+            )
             i = i + 1
-        return "" # "Block id: " + str(self.block_info.get_start_address()) + " States: " + str(len(self.state_per_instr))
+        return ""  # "Block id: " + str(self.block_info.get_start_address()) + " States: " + str(len(self.state_per_instr))
 
-class Analysis: 
 
-    def __init__(self,vertices, blockid, initialState): 
+class Analysis:
+    def __init__(self, vertices, blockid, initialState):
         self.vertices = vertices
         self.pending = [blockid]
         self.blocks_info = {}
         self.blocks_info[blockid] = BlockAnalysisInfo(vertices[blockid], initialState)
 
-    def analyze (self):
-        while (len(self.pending) > 0) :
+    def analyze(self):
+        while len(self.pending) > 0:
             block_id = self.pending.pop()
 
             # Process the block
@@ -91,59 +104,71 @@ class Analysis:
 
             block_info.process_block()
 
-            self.process_jumps(block_id,block_info.get_output_state())
+            self.process_jumps(block_id, block_info.get_output_state())
 
-    def process_jumps (self,block_id, input_state): 
+    def process_jumps(self, block_id, input_state):
         basic_block = self.vertices[block_id]
-        if basic_block.get_block_type() == "terminal": 
-            return 
+        if basic_block.get_block_type() == "terminal":
+            return
 
-        jump_target = basic_block.get_jump_target()        
-        if (jump_target != 0 and jump_target != -1) and self.blocks_info.get(jump_target) == None:
+        jump_target = basic_block.get_jump_target()
+        if (jump_target != 0 and jump_target != -1) and self.blocks_info.get(
+            jump_target
+        ) == None:
             self.pending.append(jump_target)
             # print("************")
             # print(block_id)
             # print(jump_target)
             # print(self.vertices[block_id].display())
-            self.blocks_info[jump_target] = BlockAnalysisInfo(self.vertices[jump_target], input_state)
+            self.blocks_info[jump_target] = BlockAnalysisInfo(
+                self.vertices[jump_target], input_state
+            )
 
-        elif (jump_target != 0 and jump_target != -1) and self.blocks_info.get(jump_target).revisit_block(input_state,jump_target): 
-            #print("REVISITING BLOCK!!! " + str(jump_target))
+        elif (jump_target != 0 and jump_target != -1) and self.blocks_info.get(
+            jump_target
+        ).revisit_block(input_state, jump_target):
+            # print("REVISITING BLOCK!!! " + str(jump_target))
             self.pending.append(jump_target)
 
         jump_target = basic_block.get_falls_to()
         if jump_target != None and self.blocks_info.get(jump_target) == None:
             self.pending.append(jump_target)
-            self.blocks_info[jump_target] = BlockAnalysisInfo(self.vertices[jump_target], input_state)
-        elif jump_target != None and self.blocks_info.get(jump_target).revisit_block(input_state,jump_target): 
+            self.blocks_info[jump_target] = BlockAnalysisInfo(
+                self.vertices[jump_target], input_state
+            )
+        elif jump_target != None and self.blocks_info.get(jump_target).revisit_block(
+            input_state, jump_target
+        ):
             self.pending.append(jump_target)
-                
-    def get_analysis_results(self,pc,posrel):
+
+    def get_analysis_results(self, pc, posrel):
         block = pc.split(":")[0]
-        if str(block).find("_")==-1:
+        if str(block).find("_") == -1:
             block = int(block)
         # try:
         #     block = int(block)
-        # except ValueError: 
+        # except ValueError:
         #     pass
-        id = pc.split(":")[1] 
-        return self.blocks_info[block].get_state_at_instr(int(id)+posrel)
+        id = pc.split(":")[1]
+        return self.blocks_info[block].get_state_at_instr(int(id) + posrel)
 
-    def get_block_results(self,blockid): 
+    def get_block_results(self, blockid):
         return self.blocks_info[blockid]
 
-    def __repr__(self): 
+    def __repr__(self):
         for id in self.blocks_info:
-            print(str(self.blocks_info[id]))    
+            print(str(self.blocks_info[id]))
         return ""
 
-def perform_storage_analysis(vertices, debug, jump_directions):
-    global debug_info 
-    debug_info = debug
-    
-    memory = Analysis(vertices,0, JumpOriginAbstractState(0,{}, {-1: set()}, debug, jump_directions))
-    memory.analyze()
 
+def perform_storage_analysis(vertices, debug, jump_directions):
+    global debug_info
+    debug_info = debug
+
+    memory = Analysis(
+        vertices, 0, JumpOriginAbstractState(0, {}, {-1: set()}, debug, jump_directions)
+    )
+    memory.analyze()
 
     if "costabs" not in os.listdir(global_params_ethir.tmp_path):
         os.mkdir(global_params_ethir.costabs_path)
@@ -151,36 +176,36 @@ def perform_storage_analysis(vertices, debug, jump_directions):
     name = global_params_ethir.costabs_path
 
     with open(f"{name}/saltos_storage.txt", "w") as f:
-        f.write(jump_directions + "\n")
+        f.write(str(jump_directions) + "\n")
 
     return jump_directions
-    
 
-def perform_memory_analysis(vertices, cname, csource, compblocks, fblockmap, type_analysis, debug):     
-    global debug_info 
+
+def perform_memory_analysis(
+    vertices, cname, csource, compblocks, fblockmap, type_analysis, debug
+):
+    global debug_info
 
     debug_info = debug
-    
-    set_memory_utils_globals(compblocks, fblockmap)
 
+    set_memory_utils_globals(compblocks, fblockmap)
 
     if type_analysis == "jump_origin":
         return perform_storage_analysis(vertices, debug, [])
 
-
     print("Slots analysis started!")
 
     MemoryAccesses.init_globals(csource, cname)
-    accesses = MemoryAccesses({},{},{},{},vertices)
-    
+    accesses = MemoryAccesses({}, {}, {}, {}, vertices)
+
     init_slot = memory_slots.slots_autoid
     SlotsAbstractState.initglobals(accesses)
-    slots = Analysis(vertices,0,SlotsAbstractState(set({}),{},{}))
+    slots = Analysis(vertices, 0, SlotsAbstractState(set({}), {}, {}))
     slots.analyze()
 
     print("Slots analysis finished!")
 
-    constants = Analysis(vertices,0, OffsetAnalysisAbstractState(0,{},debug_info))
+    constants = Analysis(vertices, 0, OffsetAnalysisAbstractState(0, {}, debug_info))
     constants.analyze()
 
     print("Constants analysis finished!")
@@ -188,23 +213,21 @@ def perform_memory_analysis(vertices, cname, csource, compblocks, fblockmap, typ
     print("Starting offset memory analysis " + str(cname))
 
     if type_analysis == "baseref":
-        MemoryAbstractState.initglobals(slots,accesses)
-        memory = Analysis(vertices,0, MemoryAbstractState(0,{},{}))
+        MemoryAbstractState.initglobals(slots, accesses)
+        memory = Analysis(vertices, 0, MemoryAbstractState(0, {}, {}))
         memory.analyze()
 
-    
     elif type_analysis == "offset":
-        MemoryOffsetAbstractState.init_globals(slots,accesses, constants)
-        memory = Analysis(vertices,0, MemoryOffsetAbstractState(0,{},{}))
+        MemoryOffsetAbstractState.init_globals(slots, accesses, constants)
+        memory = Analysis(vertices, 0, MemoryOffsetAbstractState(0, {}, {}))
         memory.analyze()
 
     else:
         raise Exception("Type for memory analysis uncorrect")
-        
+
     # MemoryAbstractState.initglobals(slots,accesses)
     # memory = Analysis(vertices,0, MemoryAbstractState(0,{},{}))
     # memory.analyze()
-
 
     # if debug:
     #     print("Memory results:")
@@ -217,20 +240,30 @@ def perform_memory_analysis(vertices, cname, csource, compblocks, fblockmap, typ
     #     print("\n\n")
     accesses.process_free_mstores()
 
-    print('Free memory analyss finished\n\n')
+    print("Free memory analyss finished\n\n")
 
     nslots = memory_slots.slots_autoid - init_slot
 
-    print ("SLOTS Contract " + cname + ": " + str(nslots))
-    print("Memory read accesses Contract"+ cname+": "+str(len(accesses.readset.keys())))
-    print("Memory write accesses Contract"+ cname+": "+str(len(accesses.writeset.keys())))
-    
+    print("SLOTS Contract " + cname + ": " + str(nslots))
+    print(
+        "Memory read accesses Contract"
+        + cname
+        + ": "
+        + str(len(accesses.readset.keys()))
+    )
+    print(
+        "Memory write accesses Contract"
+        + cname
+        + ": "
+        + str(len(accesses.writeset.keys()))
+    )
+
     print("********************************** INIT")
-    memopt = MemoryOptimizerConnector(accesses.readset, accesses.writeset, vertices,cname)
+    memopt = MemoryOptimizerConnector(
+        accesses.readset, accesses.writeset, vertices, cname
+    )
     memopt.process_blocks_memory(debug_info)
     # memopt.process_blocks_storage(debug_info)
     print("********************************** END")
-    
+
     return slots, memory, accesses, memopt
-
-
--- ethir/sequence.py
+++ ethir/sequence.py
@@ -1,5 +1,4 @@
 class Sequence:
-
     sequence: tuple
     instruction_index: int
     storage_value: set
@@ -12,15 +11,16 @@ class Sequence:
         self.sequence = sequence
         self.stack_position = stack_position
         self.displacement = 0
-    
+
     def register_instruction(self, instruction: str, stack: dict):
-
         if instruction.startswith(self.sequence[self.instruction_index]):
-
             # displacement in case of sload
             if self.instruction_index == 0:
-                self.displacement = stack[len(stack) -1]
-            
+                if stack.get(len(stack) - 1) is not None:
+                    self.displacement = stack[len(stack) - 1]
+                else:
+                    self.displacement = {"*"}
+
             # stack direction for sstore and sload
             if self.storage_value is None and self.instruction_index == 1:
                 stack_value = stack[len(stack) - self.stack_position]
@@ -30,12 +30,12 @@ class Sequence:
                     values = set()
                     for displacement in self.displacement:
                         values = values.union(stack_value[str(displacement)])
-                    
+
                     self.storage_value = values
                 else:
                     self.storage_value = stack_value
 
-            self.instruction_index = (self.instruction_index + 1)%len(self.sequence)
+            self.instruction_index = (self.instruction_index + 1) % len(self.sequence)
         else:
             self.instruction_index = 0
             self.storage_value = None
@@ -44,5 +44,4 @@ class Sequence:
         return self.storage_value
 
     def __repr__(self) -> str:
-        return f'Index: {self.instruction_index}, Value: {self.storage_value}'
-        
+        return f"Index: {self.instruction_index}, Value: {self.storage_value}"
