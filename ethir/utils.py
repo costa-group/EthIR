7bb6a7c44df14234df694732117b9b5b3145549b
Pablo Gordillo <pabgordi@ucm.es>
2024-01-10 11:27:20 +0100

    comment prints

--- ethir/utils.py
+++ ethir/utils.py
@@ -1081,18 +1081,19 @@ def run_gastap(contract_name, entry_functions):
 
     for bl in entry_functions:
     
-        cmd = "sh /home/groman/Systems/costa/costabs/src/interfaces/shell/costabs_shell "+global_params_ethir.costabs_path+"/costabs/"+contract_name+"_saco.rbr"+ " -entries "+"block"+str(bl) +" -ethir yes -ethir_mem no -cost_model gas -custom_out_path yes -evmcc star" 
+        cmd = "sh /home/pablo/Systems/costa/costabs/src/interfaces/shell/costabs_shell "+global_params_ethir.costabs_path+"/costabs/"+contract_name+"_saco.rbr"+ " -entries "+"block"+str(bl) +" -ethir yes -ethir_mem no -cost_model gas -custom_out_path yes" # -evmcc star" 
         FNULL = open(os.devnull, 'w')
-        print(cmd)
+        #print(cmd)
         solc_p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=FNULL)
         out = solc_p.communicate()[0].decode()
         outputs.append(out)
         ub = filter_ub(out)
-        if ub == "" or ub == None: 
-            raise Exception("Error at cooking the ub expression")
-        ubs[bl] = ub
+        if ub == "" or ub == None:
+            print("GASTAPERR: Error at cooking the ub expression. "+str(contract_name)+",block"+str(bl))
+            # raise Exception("Error at cooking the ub expression")
+        else:
+            ubs[bl] = ub
 
-        print(out)
 
     return outputs,ubs
 
